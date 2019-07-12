a6d4a0348fc662c26558688146cdcd3195835317
unknown <paul.jose.rodriguez@gmail.com>
2019-07-12 02:04:47 -0700

    Updated projects and made the app.js more concise

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', function() {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
     $navbarBurgers.forEach(el => {
       el.addEventListener('click', () => {
-
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
@@ -17,39 +17,34 @@ document.addEventListener('DOMContentLoaded', () => {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
-
       });
     });
   }
 
-  const btnsN = document.querySelectorAll('.buttons-next');
-  const btnsP = document.querySelectorAll('.buttons-previous');
-  const sections = document.querySelectorAll('.card-img');
-  btnsN[0].addEventListener('click', () => {
-    sections[1].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsN[1].addEventListener('click', () => {
-    sections[2].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsN[2].addEventListener('click', () => {
-    sections[3].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsN[3].addEventListener('click', () => {
-    sections[4].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
+  const btnsN = document.getElementsByClassName('buttons-next');
+  const btnsP = document.getElementsByClassName('buttons-previous');
+  const sections = document.getElementsByClassName('card-img');
 
-  btnsP[0].addEventListener('click', () => {
-    sections[0].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsP[1].addEventListener('click', () => {
-    sections[1].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsP[2].addEventListener('click', () => {
-    sections[2].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
-  btnsP[3].addEventListener('click', () => {
-    sections[3].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
-  });
- 
-
-})
+  for (let i = 0; i < btnsN.length; i++) {
+    let button = btnsN[i];
+    button.addEventListener('click', () => {
+      // console.log(i)
+      sections[i + 1].scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center',
+        block: 'nearest'
+      });
+    });
+  }
+  for (let i = 0; i < btnsP.length; i++) {
+    let button = btnsP[i];
+    button.addEventListener('click', () => {
+      // console.log(i)
+      sections[i-1].scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center',
+        block: 'nearest'
+      });
+    });
+  }
+});
